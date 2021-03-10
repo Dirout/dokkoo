6eee57e1c41768bd12cd728d49e3d8ce0a72ff2a
Dokkoo <Dirout@users.noreply.github.com>
2021-03-10 22:37:01 +0000

    Publish documentation to GitHub Pages

---
+++ implementors/core/fmt/trait.Display.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["dokkoo"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"struct\" href=\"dokkoo/struct.Date.html\" title=\"struct dokkoo::Date\">Date</a>","synthetic":false,"types":["dokkoo::Date"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
