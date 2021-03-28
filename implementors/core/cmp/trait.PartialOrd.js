1429bf26742a87015e527b03c3a43fab516f874f
Dokkoo <Dirout@users.noreply.github.com>
2021-03-28 05:56:53 +0000

    Publish documentation to GitHub Pages

---
+++ implementors/core/cmp/trait.PartialOrd.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["dokkoo"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a>&lt;<a class=\"struct\" href=\"dokkoo/struct.Date.html\" title=\"struct dokkoo::Date\">Date</a>&gt; for <a class=\"struct\" href=\"dokkoo/struct.Date.html\" title=\"struct dokkoo::Date\">Date</a>","synthetic":false,"types":["dokkoo::Date"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
