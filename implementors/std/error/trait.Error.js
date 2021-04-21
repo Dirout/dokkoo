2563523e7a6d4c89f330b45b75344fc2e9660b5c
Dokkoo <Dirout@users.noreply.github.com>
2021-04-21 18:41:06 +0000

    Publish documentation to GitHub Pages

---
+++ implementors/std/error/trait.Error.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["dokkoo"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"struct\" href=\"dokkoo/struct.Date.html\" title=\"struct dokkoo::Date\">Date</a>","synthetic":false,"types":["dokkoo::Date"]},{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"struct\" href=\"dokkoo/struct.Page.html\" title=\"struct dokkoo::Page\">Page</a>","synthetic":false,"types":["dokkoo::Page"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
