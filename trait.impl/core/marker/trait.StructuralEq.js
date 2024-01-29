a234a6085ce49c0707966ff4f9eaa5f93652a8e3
Dokkoo <Dirout@users.noreply.github.com>
2024-01-29 03:54:16 +0000

    Publish documentation to GitHub Pages

--- trait.impl/core/marker/trait.StructuralEq.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {
-"dokkoo":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.StructuralEq.html\" title=\"trait core::marker::StructuralEq\">StructuralEq</a> for <a class=\"struct\" href=\"dokkoo/struct.Page.html\" title=\"struct dokkoo::Page\">Page</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.StructuralEq.html\" title=\"trait core::marker::StructuralEq\">StructuralEq</a> for <a class=\"struct\" href=\"dokkoo/struct.Date.html\" title=\"struct dokkoo::Date\">Date</a>"]]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
